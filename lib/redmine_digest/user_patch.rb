d3e758614656bbaad682af9ff56559bf9b655feb
Alexander Morozov <morozov.18.09@gmail.com>
2016-04-25 15:20:22 +0300

    Moved to unloadable concern

--- lib/redmine_digest/user_patch.rb
+++ lib/redmine_digest/user_patch.rb
@@ -1,27 +1,16 @@
+require "active_support/concern"
 module RedmineDigest
   # Patches Redmine's Users dynamically.  Adds a relation to digest_account.
   module UserPatch
-    def self.included(base) # :nodoc:
-      base.extend(ClassMethods)
+    extend ActiveSupport::Concern
 
-      base.send(:include, InstanceMethods)
-
-      # Same as typing in the class
-      base.class_eval do
+    included do
         unloadable # Send unloadable so it will not be unloaded in development
 
         has_one :digest_account
         accepts_nested_attributes_for :digest_account
         safe_attributes 'digest_account'
         safe_attributes 'digest_account_attributes'
-      end
-
-    end
-
-    module ClassMethods
-    end
-
-    module InstanceMethods
     end
   end
 end
